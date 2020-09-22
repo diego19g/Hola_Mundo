b98dc3e437096dbd7a28d3bcf1cdcb5c53526817
diego19g <diego19rm@gmail.com>
2020-09-22 20:17:41 +0200

    commit

---
+++ prueba.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+            $a=12;
+            $b="9.99";
+            $c="ejemplo variables PHP";
+            //impresion
+            echo $a."<br>".$b."<br>".$c;
+            $mivariable="prueba git merge";
+            $otra=36;
+        ?>
+    </body>
+</html>
